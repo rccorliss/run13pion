d6ff597d194ba9d5c87e9e722f15a3001624380c
Ross Corliss <rcorliss@mit.edu>
2020-04-06 01:47:43 -0400

    added division into three zdc ratio bins

--- rcc_draw_all_plots.C
+++ rcc_draw_all_plots.C
@@ -9,8 +9,10 @@ void rcc_draw_all_plots()
   double pt_limits[nptbins+1] = {1, 1.5, 2, 2.5, 3, 4, 5, 6, 7, 8, 12};
   const int nzdcbins=3;
   double zdc_limits[nzdcbins+1]={0,2e9,4e9,1e10};
-  const int nspinpats=30;
-
+  const int nratiobins=3;
+  double ratio_limits[nratiobins+1]={0.8,0.95,1.03,1.2};
+  const int nspinpats=10;
+  const int spinpat_lower_limit=20;
 
   
   TString filename;
@@ -36,27 +38,34 @@ void rcc_draw_all_plots()
   float m_zdcsum,m_zdcratio;
   int m_spinpat,m_run;
   float m_bpol,m_ypol;
+  int m_i;//increments by one every tiem we fill.  Unique run ID, but not run number.
   mtree->Branch("zdcsum",&m_zdcsum);
   mtree->Branch("zdcratio",&m_zdcratio);
   mtree->Branch("spinpat",&m_spinpat);
   mtree->Branch("run",&m_run);
   mtree->Branch("bpol",&m_bpol);
   mtree->Branch("ypol",&m_ypol);
+  mtree->Branch("index",&m_i);
   
 
   
-  TH1F *hWeightedAsym=new TH1F("hWeightedAsym","Weighted average asymmetry vs pt",nptbins,pt_limits);
-  TH1F *hWeightSum=new TH1F("hWeightSum","Sum of Weights vs pt",nptbins,pt_limits);
+  TH1F *hWeightedAsym=new TH1F("hWeightedAsym","Weighted average asymmetry vs pt;pt [GeV];asym",nptbins,pt_limits);
+  TH1F *hWeightSum=new TH1F("hWeightSum","Sum of Weights vs pt;pt [GeV];weight",nptbins,pt_limits);
   hWeightedAsym->Sumw2();
 
-  //accumulators for asymmetry divided into adc bins
+  //accumulators for asymmetry divided into zdc bins
   TH2F *hWeightedAsymByLumi=new TH2F("hWeightedAsymByLumi","Weighted average asymmetry vs pt",nzdcbins,zdc_limits,nptbins,pt_limits);
-  TH2F *hWeightSumByLumi=new TH2F("hWeightSumByLumi","Sum of Weights vs pt",nzdcbins,zdc_limits,nptbins,pt_limits);
+  TH2F *hWeightSumByLumi=new TH2F("hWeightSumByLumi","Sum of Weights vs pt",nratiobins,ratio_limits,nptbins,pt_limits);
+
+   //accumulators for asymmetry divided into zdc bins
+  TH2F *hWeightedAsymByRatio=new TH2F("hWeightedAsymByRatio","Weighted average asymmetry vs pt and zdc ratio",nzdcbins,zdc_limits,nptbins,pt_limits);
+  TH2F *hWeightSumByRatio=new TH2F("hWeightSumByRatio","Sum of Weights vs pt and zdc ratio",nratiobins,ratio_limits,nptbins,pt_limits);
 
   //accumulators for asymmetry divided into spin patterns bins
-  TH2F *hWeightedAsymBySpinpat=new TH2F("hWeightedAsymBySpinpat","Weighted average asymmetry vs pt",30,0.5,30.5,nptbins,pt_limits);
-  TH2F *hWeightSumBySpinpat=new TH2F("hWeightSumBySpinpat","Sum of Weights vs pt",30,0.5,30.5,nptbins,pt_limits);
-  
+  TH2F *hWeightedAsymBySpinpat=new TH2F("hWeightedAsymBySpinpat","Weighted average asymmetry vs pt;spinpat;pt [GeV]",30,0.5,30.5,nptbins,pt_limits);
+  TH2F *hWeightSumBySpinpat=new TH2F("hWeightSumBySpinpat","Sum of Weights vs pt",nspinpats,spinpat_lower_limit-0.5,spinpat_lower_limit+nspinpats-0.5,nptbins,pt_limits);
+
+  m_i=0;
   for (int i=0;i<maxptbins;i++)
     asymByBin[i]=&(rawAsym[i*maxruns]);
 
@@ -106,7 +115,7 @@ void rcc_draw_all_plots()
     m_bpol=hPol->GetMean(1);
     m_ypol=hPol->GetMean(2);
     mtree->Fill();
-
+    m_i++;
     
     //two things I can do:
     //1) plot vs pt and runnumber
@@ -132,6 +141,8 @@ void rcc_draw_all_plots()
       //add this to a weighted sum for the zdc bin of choice:
       hWeightedAsymByLumi->Fill(lumi,ptmid,asym*w);
       hWeightSumByLumi->Fill(lumi,ptmid,w);
+      hWeightedAsymByRatio->Fill(m_zdcratio,ptmid,asym*w);
+      hWeightSumByRatio->Fill(m_zdcratio,ptmid,w);
       hWeightedAsymBySpinpat->Fill(spinpat,ptmid,asym*w);
       hWeightSumBySpinpat->Fill(spinpat,ptmid,w);
       
@@ -150,6 +161,7 @@ void rcc_draw_all_plots()
   hWeightedAsym->Divide(hWeightSum);
   hWeightedAsymByLumi->Divide(hWeightSumByLumi);
   hWeightedAsymBySpinpat->Divide(hWeightSumBySpinpat);
+  hWeightedAsymByRatio->Divide(hWeightSumByRatio);
 
   //fix the errorbars:
   for (int i=0;i<nptbins;i++){
@@ -161,11 +173,10 @@ void rcc_draw_all_plots()
   }
 
   //fix the error bars on the 2D plots and extract their 1D components, so they can be more easily visualized:
-  TH1F *hFinalAsymBySpinpat[nspinpats];
   TH1F *hFinalAsymByLumi[nzdcbins];
   for (int j=0;j<nzdcbins;j++){
     hFinalAsymByLumi[j]=new TH1F(Form("hFinalAsymByLumi%d",j),
-				 Form("Weighted asymmetry vs pt for runs with %f<zdc_narrow<=%f",
+				 Form("Weighted asymmetry vs pt for runs with %f<zdc_narrow<=%f;pt [GeV];asym",
 				      zdc_limits[j],zdc_limits[j+1]),
 				 nptbins,pt_limits);
     for (int i=0;i<nptbins;i++){
@@ -178,9 +189,28 @@ void rcc_draw_all_plots()
       hFinalAsymByLumi[j]->SetBinError(i+1,err);
     }
   }
+
+  TH1F *hFinalAsymByRatio[nratiobins];
+  for (int j=0;j<nratiobins;j++){
+    hFinalAsymByRatio[j]=new TH1F(Form("hFinalAsymByRatio%d",j),
+				 Form("Weighted asymmetry vs pt for runs with %f<unlike/like<=%f;pt [GeV];asym",
+				      ratio_limits[j],ratio_limits[j+1]),
+				 nptbins,pt_limits);
+    for (int i=0;i<nptbins;i++){
+      float sumweight=hWeightSumByRatio->GetBinContent(j+1,i+1);
+      float asym=hWeightedAsymByRatio->GetBinContent(j+1,i+1);
+      if (sumweight==0) continue; //skip if we have no weight in this bin.
+      //err_on_average=sqrt(1/sum of weights) as long as we pick weight=1/err2;
+      float err=sqrt(1/sumweight);
+      hFinalAsymByRatio[j]->SetBinContent(i+1,asym);
+      hFinalAsymByRatio[j]->SetBinError(i+1,err);
+    }
+  }
+
+  TH1F *hFinalAsymBySpinpat[nspinpats];
   for (int j=0;j<nspinpats;j++){
     hFinalAsymBySpinpat[j]=new TH1F(Form("hFinalAsymBySpinpat%d",j),
-				 Form("Weighted asymmetry vs pt for runs with spinpat=%d",j),
+				 Form("Weighted asymmetry vs pt for runs with spinpat=%d;pt [GeV];asym",j+spinpat_lower_limit),
 				 nptbins,pt_limits);
     for (int i=0;i<nptbins;i++){
       float sumweight=hWeightSumBySpinpat->GetBinContent(j+1,i+1);
@@ -214,7 +244,9 @@ void rcc_draw_all_plots()
     //if (hWeightSumByLumi[j]->GetBinContent(1,1)>0)
       hFinalAsymByLumi[j]->Write();
   }
-
+  for (int j=0;j<nratiobins;j++){
+      hFinalAsymByRatio[j]->Write();
+  }
   mtree->Write();
   outfile->Close();
   return;
