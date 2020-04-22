2d27cbdfb6ad96ca92284fa89f201bfd0dc030f2
Ross Corliss <rcorliss@mit.edu>
2020-04-22 03:35:04 -0400

    compile fixes

--- rcc_gen_yield.C
+++ rcc_gen_yield.C
@@ -354,7 +354,7 @@ void InitOutput(int runnum, const char* outputdir){
   rccRunTree->Branch("npi0",&rccTotGoodClust);
   rccRunTree->Branch("nptbins",&rccNbins);
   rccRunTree->Branch("nbounds",&rccNbounds);
-  rccRunTree->Branch("ptbound",&rccBounds[0],"ptbound[nbounds]/F");
+  rccRunTree->Branch("ptbound",(void *)(&rccBounds[0]),"ptbound[nbounds]/F",32000);
 
   rccBunchTree=new TTree("bunchTree","per-bunch total raw and good clusters per det and sum");
   rccBunchTree->Branch("nRawByPt",rccRawClustPtr,Form("nRawByPt[%d]/F",NPTBINS));
