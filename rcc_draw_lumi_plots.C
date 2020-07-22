8d1f5dfe3740fe492d4ffa84fd7149f65dec0405
Ross Corliss <rcorliss@mit.edu>
2020-07-22 05:04:48 -0400

    minor

--- rcc_draw_lumi_plots.C
+++ rcc_draw_lumi_plots.C
@@ -82,11 +82,11 @@ void rcc_draw_lumi_plots(){
     c=new TCanvas(Form("c%d",nc),Form("c%d",nc),800,600);
     c->Divide(2,2);
     c->cd(1);
-    t->Draw("star_bbcwidecnt/gl1_bbcwidelive",rcc_cross_qa+rcc_clip_loud_runs,"colz");
+    t->Draw("star_bbcwiderun/gl1_bbcwidelive",rcc_cross_qa+rcc_clip_loud_runs,"colz");
     c->cd(2);
-    t->Draw("star_bbc30cnt/gl1_bbc30live",rcc_cross_qa+rcc_clip_loud_runs,"colz");
+    t->Draw("star_bbc30run/gl1_bbc30live",rcc_cross_qa+rcc_clip_loud_runs,"colz");
     c->cd(3);
-    t->Draw("star_zdcwidecnt/gl1_zdcwidelive",rcc_cross_qa+rcc_clip_loud_runs,"colz");
+    t->Draw("star_zdcwiderun/gl1_zdcwidelive",rcc_cross_qa+rcc_clip_loud_runs,"colz");
     c->cd(4);
     //t->Draw("star_zdc30cnt/gl1_zdc30live",rcc_cross_qa+rcc_clip_loud_runs,"colz");
     nc++;
