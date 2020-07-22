d807a0f6ce1ed32b8f8de53191b6b0c03a765852
Ross Corliss <rcorliss@mit.edu>
2020-07-22 05:12:25 -0400

    minor

--- rcc_draw_lumi_plots.C
+++ rcc_draw_lumi_plots.C
@@ -79,14 +79,20 @@ void rcc_draw_lumi_plots(){
 
   //show the SetB parameters we're cutting on.
   if (1){
-    c=new TCanvas(Form("c%d",nc),Form("c%d",nc),1200,500);
-    c->Divide(3,1);
+    c=new TCanvas(Form("c%d",nc),Form("c%d",nc),1200,800);
+    c->Divide(3,2);
     c->cd(1)->SetLogy();
     t->Draw("star_bbcwiderun/gl1_bbcwidelive",rcc_cross_qa+rcc_clip_loud_runs+"star_cross==0","colz");
     c->cd(2)->SetLogy();
     t->Draw("star_bbc30run/gl1_bbc30live",rcc_cross_qa+rcc_clip_loud_runs+"star_cross==0","colz");
     c->cd(3)->SetLogy();
     t->Draw("star_zdcwiderun/gl1_zdcwidelive",rcc_cross_qa+rcc_clip_loud_runs+"star_cross==0","colz");
+   c->cd(4)->SetLogy();
+    t->Draw("star_bbcwiderun/gl1_bbcwidelive","star_clkrun"*rcc_cross_qa+rcc_clip_loud_runs+"star_cross==0","colz");
+    c->cd(5)->SetLogy();
+    t->Draw("star_bbc30run/gl1_bbc30live","star_clkrun"*rcc_cross_qa+rcc_clip_loud_runs+"star_cross==0","colz");
+    c->cd(6)->SetLogy();
+    t->Draw("star_zdcwiderun/gl1_zdcwidelive","star_clkrun"*rcc_cross_qa+rcc_clip_loud_runs+"star_cross==0","colz");
     // c->cd(4)->SetLogy();
     //t->Draw("star_zdc30cnt/gl1_zdc30live",rcc_cross_qa+rcc_clip_loud_runs,"colz");
     nc++;
