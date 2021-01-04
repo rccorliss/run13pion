e192da853979eec36fd5f46364d250ce0a8f8fa0
Ross Corliss <rcorliss@mit.edu>
2021-01-04 03:19:00 -0500

    changed Run_rcc_gen_yield.C to use dec2020 anatrain output and runlist.

--- Run_rcc_gen_yield.C
+++ Run_rcc_gen_yield.C
@@ -13,8 +13,8 @@ void Run_rcc_gen_yield()
   gROOT->ProcessLine(".L rcc_gen_yield.C+");
   gROOT->ProcessLine(".L rcc_calc_all.C+");
   //ifstream listfile("Run11_singlefile.txt");
-  ifstream listfile("/phenix/spin2/pmontu/offline/analysis/pmontu/"
-                    "relative_luminosity/macros/final_run_list.txt");
+  //ifstream listfile("/phenix/spin2/pmontu/offline/analysis/pmontu/relative_luminosity/macros/final_run_list.txt");
+  ifstream listfile("rcc_runlist_jan2021.txt");
   int filename;
   //GenALL(runnum,minbias);
   while (listfile.good()){
@@ -22,6 +22,7 @@ void Run_rcc_gen_yield()
     printf("loading %d\n",filename);
     //uncomment the following line to regenerate the yield files:
     //rcc_gen_yield(filename,"/phenix/spin/phnxsp01/rosscorliss/taxi/Run13pp510MPC/15944/data/","./yields/");
+    rcc_gen_yield(filename,"/phenix/spin/phnxsp01/rosscorliss/taxi/Run13pp510MPC/16882/data/","./yields/");
     rcc_calc_all(filename,"./yields/","./asyms/");
       
    
